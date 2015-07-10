877e021df65ef3f37bc1fc3f9dde9a9342d92c02
Marcelo Jara Almeyda <marjara35@gmail.com>
2015-07-10 00:22:44 -0300

    Reformat writer.

--- src/writer.py
+++ src/writer.py
@@ -1,7 +1,6 @@
 import os
 
 class Writer(object):
-
     def __init__(self, level, root):
         self.level = level
         self.root = root
@@ -9,12 +8,15 @@ class Writer(object):
     def write(self):
         top = self.gen_top()
         left = self.gen_left()
+
         directory = '%dx%d' % (self.level.width, self.level.height)
         path = os.path.join(self.root, directory)
+
         if not os.path.exists(path):
             os.mkdir(path)
+
         file_name = self.level.id + '.krk'
-        print os.path.join(path, file_name)
+
         with open(os.path.join(path, file_name), 'w') as f:
             f.write(top)
             f.write('\n===================\n')
@@ -29,8 +31,10 @@ class Writer(object):
                 value = self.level.get_top_at(x, y)
                 if y == self.level.top_margin - 1 and sum(self.level.get_col(x)) == 0:
                     row.append('0')
+
                 else:
                     row.append(str(value) if value != -1 else '')
+
                 if value != -1:
                     has_one_value = True
 
@@ -63,6 +67,7 @@ class Writer(object):
         for row in rows:
             if row.count('') == len(row):
                 row[-1] = '0'
+
             left.append('\t'.join(row))
 
         return '\n'.join(left)
